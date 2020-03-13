558b29c2d9b6539e74a4d87bae828870e8e5633e
Luanzheng Guo <guo7@llnl.gov>
2020-03-13 02:55:44 -0700

    bug fixed

---
+++ miniFE/src/nodefi.sh
@@ -0,0 +1,1 @@
+salloc -N 3 --ntasks-per-node=4 --time=00:28:00 -ppdebug mpirun -np 8 --mca orte_enable_recovery 1 --mca pml ^cm --mca plm_slurm_args "--wait=0" ./miniFE.x -nx 66 -ny 64 -nz 64 -cp 1 -cp2f -nodefi
