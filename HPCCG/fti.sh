a0ea707bf9205803b50ac1e3cc13d23251cc3246
Luanzheng Guo <guo7@llnl.gov>
2020-05-10 03:59:53 -0700

    fix typos

--- HPCCG/fti.sh
+++ HPCCG/fti.sh
@@ -1,1 +1,2 @@
-salloc -N 4 --ntasks-per-node=2 --time=00:10:00 -ppdebug mpirun -np 8 --mca orte_enable_recovery 1 --mca pml ^cm --mca plm_slurm_args "--wait=0" ./test_HPCCG config.L1.fti 64 64 128 10 -level 1 
+salloc -N 4 --ntasks-per-node=2 --time=00:10:00 -ppdebug mpirun -np 8 --mca orte_enable_recovery 1 --mca pml ^cm --mca plm_slurm_args "--wait=0" ./test_HPCCG config.L1.fti 64 64 128 10 -level 1 -procfi 2>&1 | tee debug.txt
+
